0a310e92f2a6a92f4493a97e77c11a1ab62cd05f
artemfeof <1227106@nvna.eu>
2020-09-19 09:44:57 +0300

    updating index.php file

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
 echo "Hello world!";
+$username = "artemfeof";
+echo $username;
+$age = "24";
+echo $age;
+$isTrue = true;
+echo $isTrue."<br>";
+echo "My name is $username<br>";
+$usernameArray = ["artemfof", "bla1"];
+echo $usernameArray [1];
+$usernameAsocArray = ["username" => "artemfeof", "age" => 24];
+echo $usernameAsocArray  ["age"];
+for ($i = 0; $i < 2; $i++){
+    echo $usernameArray [$i];
+};
+foreach ($usernameAsocArray as $key => $item){
+  echo $key." ".$item."<br>";
+};
